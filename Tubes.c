aa9f5b7bf924f931d7dac88a26219d881cd04898
joshsoeros <premanajyothi@gmail.com>
2025-05-07 21:34:36 +0700

    perubahannya ada di fitur balababds

--- Tubes.c
+++ Tubes.c
@@ -1,8 +1,58 @@
 #include<stdio.h>
 int main(){
-    printf("Hello world");
+    int n;
+    
+    switch (n)
+    {
+    printf("pilih menu 1:"); scanf("%d", n);
+    case 1:
+    __ANONYMOUS_DEFINED
 
-    int a = 10, b = 10;
-    printf("Hasilnya adalah : %d", a + b);
-    return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    break;
+
+    case 2:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    break;       
+
+    default:
+        break;
+    }
 }
