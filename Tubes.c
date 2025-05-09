01471c4da40e4b572dbffb3598e6669a6cf5e32a
joshsoeros <premanajyothi@gmail.com>
2025-05-09 10:52:07 +0700

    baru sampe regis

--- Tubes.c
+++ Tubes.c
@@ -1,18 +1,45 @@
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+struct akun
+{
+    char username[100], password[100];
+} inp;
+
+void regis(){
+    printf("Registrasi Akun!\n");
+    printf("Masukkan Username\t: "); scanf("%s", inp.username);
+    printf("Masukkan Password\t: "); scanf("%s", inp.password);
+    system("pause");
+    system("cls");
+}
+void login(){
+    char inputus[100], inputpas[100];
+    int i;
+    printf("Login Akun!\n");
+    printf("Masukkan Username\t: "); scanf("%s", inputus);
+    printf("Masukka Password\t: "); scanf("%s", inputpas);
+    if (strcmp(inputus,inp.username)== 0 && strcmp(inputpas,inp.password)== 0){
+        printf("Selamat Datang di D'Tour!\n");
+    } else
+    {
+    }
+}
 
 int main(){
     int n;
-
-    printf("Pilih menu (1. Admin / 2. User) : "); scanf("%d", &n) ;
+    printf("Pilih menu (1. Admin/ 2. User) : "); scanf("%d", &n);
     switch (n)
     {
     case 1:
-        printf("Menu Admin :");
+        printf("Menu admin :");
         break;
     case 2:
-        printf("Menu User : ");
-        break;
+        regis();
+        login();
     default:
         break;
     }
+
 }
