55a42c1b612ffa0ef2f00330f12d9a5e8ee7608b
joshsoeros <premanajyothi@gmail.com>
2025-05-21 23:21:59 +0700

    masih ada error

--- Tubes.c
+++ Tubes.c
@@ -1,81 +1,161 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<string.h>
+#include<stdlib.h>
+#include<stdbool.h>
 
-
-struct dataAdmin {
+//deklarasi variabel struct dan akun
+struct akun {
     char username[100], password[100];
-}data;
+};
+FILE *data_akun;
 
 
-//List fungsi
-void loginAdmin(); 
-void menuAdmin();
+
+//deklarasi nama File
 void regisUser();
+void loginUser();
+void menuUser();
+int main();
+
+
+void regisUser(){
+    system("cls");
+    data_akun = fopen("data_akun.dat", "ab");
+    struct akun regisUs;
+    printf("==Registrasi Akun User==\n\n");
+    printf("Masukkan Username : "); gets(regisUs.username);
+    printf("Masukkan Password : "); gets(regisUs.password);
+
+    fwrite(&regisUs , sizeof(struct akun), 1 , data_akun);
+    printf("\n");
+    printf("Akun berhasil ditambahkan!!\n");
+    system("pause");
+
+    fclose(data_akun);
+    loginUser();
+}
+
+void loginUser(){
+    data_akun = fopen("data_akun.dat", "rb");
+    struct akun login, data_acc;
+
+    system("cls");
+    printf("==Login Akun User==\n\n");
+    printf("Masukkan Username : "); gets(login.username);
+    printf("Masukkan Password : "); gets(login.password);
+    printf("\n");
+
+    while (fread(&data_acc, sizeof(struct akun), 1 , data_akun) != 0) {
+        if (strcmp(login.username, data_acc.username) == 0 &&
+            strcmp(login.password, data_acc.password)== 0) {
+            printf("Login berhasil! Selamat datang di DTour %s!!!\n", login.username);
+            system("pause");
+            menuUser();
+            break;
+        } 
+    } 
+    
+    printf("Username tidak ditemukan, silahkan registrasi terlebih dahulu!!\n");
+    system("pause");    
+    fclose(data_akun);
+}
+
+void menuAdmin(){
+    int mA;
+    system("cls");
+    printf("Menu Utama :\n");
+    printf("1. Menambah Trip\n");
+    printf("2. Melihat Jenis Trip\n");
+    printf("3. Melihat Penghasilan\n");
+    printf("4. Melihat Akun Customer\n");
+    printf("5. Melihat Penghasilan\n");
+    printf("6. Log Out\n");
+
+    printf("Pilih Menu : "); scanf("%d", &mA);
+    getchar();
+    switch (mA)
+    {
+    case 1:
+        break;
+    case 2:
+        break;
+    
+    case 3:
+        break;
+    case 4:
+        break;
+    case 5:
+        break;
+    case 6:
+        printf("Anda berhasil logout sampai jumpa kembali..\n");
+        system("cls");
+        main();
+    default:
+        break;
+    }
+
+}
 
 void loginAdmin(){
-    int n = 3;
-    char usrAdmin[100], passAdmin[100];
-    getchar();
-    system("cls");
-    while (n > 0){
-        printf("Masukkan Username : "); gets(usrAdmin);
-        printf("Masukkan Password : "); gets(passAdmin);
-        if (strcmp(usrAdmin, "admin") == 0 && strcmp(passAdmin, "admin123") == 0){
-            printf("Login Berhasil! Silahkan masuk! ");
-            menuAdmin();
-            break;
-        }
-        else {
-            printf("Login Gagal! Silahkan coba lagi\n");
-            n--;
-            if (n == 0) {
-                printf("Kesempatan login sudah habis!!");
+    char usAdmin[50], pasAdmin[50];
+    const char Adminusn[50]  = "admin";
+    const char Adminpas[50]  = "admin123";
+    int chance = 3;
+
+    printf("Username Admin : "); gets(usAdmin);
+    printf("Password Admin : "); gets(pasAdmin);
+
+    if (strcmp(usAdmin, Adminusn)== 0 && strcmp(pasAdmin, Adminpas)== 0)
+    {
+        printf("Selamat datang Admin di D'Tour!!\n");
+        menuAdmin();
+    } else {
+        while (chance >= 0)
+        {
+            if (chance == 0){
+                printf("Kesempatan anda sudah habis, silahkan refresh.\n");
                 break;
-            }
-            printf("Kesempatan login tersisa %d \n", n);
+            } 
+            printf("Username dan password anda salah silahkan coba lagi!!\n");
+            printf("Kesempatan anda tersisa %d\n", chance); 
+            printf("Username Admin : "); gets(usAdmin);
+            printf("Password Admin : "); gets(pasAdmin);
+            if (strcmp(usAdmin, Adminusn)== 0 && strcmp(pasAdmin, Adminpas)== 0){
+            printf("Selamat datang Admin di D'Tour!!");
+            menuAdmin();
+            } 
+            chance--;
             printf("\n");
         }
     }
 }
 
-void menuAdmin(){
-    system("cls");
-    printf("Babi lu");
+void menuUser(){
+    printf("Mantap");
 }
 
-void regisUSer(){
-    FILE *data_akun;
-    
-}
-
-
-//Menu Main
 int main(){
     int n;
 
     system("cls");
-    printf("Pilih Menu : \n");
+    printf("Menu Utama :\n");
     printf("1. Login Admin\n");
     printf("2. Registrasi User\n");
     printf("3. Login User\n");
-    printf("Input Pilihan : "); scanf("%d", &n);
-    
+    printf("Pilih Menu : "); scanf("%d", &n);
+    getchar();
     switch (n)
     {
-    case 1 :
+    case 1:
         loginAdmin();
         break;
-    case 2 :
-        printf("2. Registrasi User");
+    case 2:
+        regisUser();
         break;
-    case 3 :
-        printf("3. Login User");
-        break;    
+    case 3:
+        loginUser();
+        break;
     default:
-        printf("Pilihan tidak ada dimenu, silahkan pilih ulang!");
-        system("pause");
-        system("cls");
         break;
     }
-}   
+}
