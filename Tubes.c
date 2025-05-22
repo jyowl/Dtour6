89e42228e6239d806e4a9e77edc42a9c9c778218
FaridES89 <evansyafarfarid@gmail.com>
2025-05-22 10:57:31 +0700

    Update Tubes.c

--- Tubes.c
+++ Tubes.c
@@ -183,4 +183,32 @@ int main(){
 void menuUser(){
     system("cls");
     printf("Menu User :\n");
+    printf("1. Melihat Jenis Trip\n");
+    printf("2. Memesan Trip\n");
+    printf("3. Pembayaran\n");
+    printf("4. Top Up Saldo\n");
+    printf("5. Riwayat Trip\n");
+    printf("6. Log Out\n");
+
+    printf("Pilih Menu : "); scanf("%d", &mU);
+    getchar();
+    switch (mU)
+    {
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    case 5:
+        break;
+    case 6:
+        printf("Anda berhasil logout sampai jumpa kembali..\n");
+        system("cls");
+        main();
+    default:
+        break;
+    }
 }
